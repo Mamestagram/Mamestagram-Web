5daa7d4dac4a20c13068609ff7fbec560734b8d2
basshhii0610 <kachakun0610@gmail.com>
2024-09-06 23:42:15 +0900

    ファイル追加、一部引き継ぎ

--- scripts/modules/pool.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const host = process.env.DB_HOST, user = process.env.DB_USER, password = process.env.DB_PASS, database = process.env.DB_NAME, port = process.env.DB_PORT;
-const mysql = require("mysql");
-const pool = mysql.createPool({ host, user, password, database, port, stringifyObjects: true });
-module.exports = pool;
