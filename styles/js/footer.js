725f2d4af8140acd39e36e09fca45239a893907d
basshhii0610 <kachakun0610@gmail.com>
2024-09-18 00:59:03 +0900

    footer修正

--- styles/js/footer.js
+++ styles/js/footer.js
@@ -2,13 +2,13 @@ $(function() {
     $("footer").hover(
         function() {
             $(this).addClass("hovered");
-            $("html, body").animate({
+            $("html, body").stop().animate({
                 scrollTop: $(window).scrollTop() + 500
             }, 500);
         },
         function() {
             $(this).removeClass("hovered");
-            $("html, body").animate({
+            $("html, body").stop().animate({
                 scrollTop: $("footer").offset().top + $("footer").height() - $(window).height()
             }, 500);
         }
